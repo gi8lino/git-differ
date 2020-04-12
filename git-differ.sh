cfba680b485402b5c88c03e2bb4aa14afcd36796
giotto <giottolino@gmail.com>
2020-04-12 03:05:47 +0200

    v1.0.1

--- git-differ.sh
+++ git-differ.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 
-VERSION="v1.0.0"
+VERSION="v1.0.1"
 
 ShowHelp() {
     printf "%s\n"  \
            "Usage: git-differ.sh [-r|--recursive] PATH [PATH ...] | [-h|--help] | [-v|--version]" \
-           "Perform a 'git diff' in 'PATH' , or in multiple 'PATH(s)'." \
+           "" \
+           "Perform a 'git diff' in 'PATH' , or in multiple 'PATHS'." \
+           "" \
            "All parameters starting with '--' will be pass to the 'git diff' command." \
            "Default 'git diff' parameter is '--stat'." \
            "" \
            "positional arguments:" \
-           "PATH...                 path(s) to perform a 'git diff'" \
+           "PATH...                 path or multiple paths to perform a 'git diff'" \
            "" \
            "parameters:" \
            "-r, --recursive         iterate over directories in PATH(s) recursively" \
+           "-e, --exclude           exclude directory(s) for checking for 'git diff'" \
+           "                        list of strings, separated by a space and" \
+           "                        surrounded by quotes (case sensitive)" \
            "-h, --help              display this help and exit" \
            "-v, --version           output version information and exit" \
            "" \
            "created by gi8lino (2020)" \
-           "https://github.com/gi8lino/git-differ"
+           "https://github.com/gi8lino/git-differ" \
+           ""
     exit 0
 }
 
@@ -27,6 +33,8 @@ walk() {
     local _current_dir="${1%/}"  # remove ending slash if set
     local _diff_params="$2"
     local _recursive="$3"
+
+    for exclude in $EXCLUDES; do [ -z "${_current_dir##*$exclude*}" ] && return; done
 
     if [ -d "${_current_dir}/.git/" ]; then
         git --git-dir=$_current_dir/.git --work-tree=$_current_dir/ diff --quiet; nochanges=$?
@@ -43,6 +51,7 @@ walk() {
     [ ! -z "$_recursive" ] && \
         for entry in "$_current_dir"/*; do [ -d "$entry" ] && walk "$entry" "$_diff_params" "$_recursive"; done
 }
+
 while [ $# -gt 0 ];do
     key="$1"
     key="${key#"${key%%[![:space:]]*}"}"  # remove leading whitespace
@@ -50,6 +59,11 @@ while [ $# -gt 0 ];do
 	    -r|--recursive)
 	    RECURSIVE=true
 	    shift
+	    ;;
+        -e|--exclude)
+	    EXCLUDES="$2"
+	    shift
+        shift
 	    ;;
 	    -v|--version)
 	    printf "git-differ.sh version: %s\n" "${VERSION}"
@@ -71,7 +85,7 @@ while [ $# -gt 0 ];do
     esac
 done
 
-[ -z "$PATHS" ] && PATHS="*/"
+[ -z "$PATHS" ] && PATHS="*/"  # if no directory was given add current directory
 [ -z "$DIFF_PARAMS" ] && DIFF_PARAMS="--stat"  # check if git diff parameter was set
 
 printf "use git diff parameters '%s'\n" "$DIFF_PARAMS"
